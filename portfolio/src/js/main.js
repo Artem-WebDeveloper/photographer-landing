c01111384496ce8028253173f8653c542675ac31
Artem-WebDeveloper <ave_art@icloud.com>
2025-09-30 23:13:39 +0300

    feat: add modals

--- portfolio/src/js/main.js
+++ portfolio/src/js/main.js
@@ -3,6 +3,8 @@ import '../styles/main.scss';
 import './MobileMenu.js';
 import MobileMenu from './MobileMenu.js';
 import Accordion from './Accordion.js';
+import BookForm from './BookForm.js';
 
 const mobileMenu = new MobileMenu();
 const accordion = new Accordion();
+const bookForm = new BookForm();
