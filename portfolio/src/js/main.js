6760c925d01626db0ce7c6b566753fb34dc40f29
Artem-WebDeveloper <ave_art@icloud.com>
2025-10-03 19:05:38 +0300

    add: slider functional

--- portfolio/src/js/main.js
+++ portfolio/src/js/main.js
@@ -5,8 +5,10 @@ import MobileMenu from './MobileMenu.js';
 import Accordion from './Accordion.js';
 import BookForm from './BookForm.js';
 import ScrollButton from './ScrollButton.js';
+import Slider from './Slider.js';
 
 new MobileMenu();
 new Accordion();
 new BookForm();
 new ScrollButton();
+new Slider();
